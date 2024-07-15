65ce005c2d585c50438e79040a7ad2850db09c46
n03an <nouman.memon@icloud.com>
2024-07-15 21:13:11 +0530

    [proj2] implement graident descent for cross entrophy loss

--- Project 2 Digit Recognition/mnist/part1/main.py
+++ Project 2 Digit Recognition/mnist/part1/main.py
@@ -41,7 +41,7 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
+# print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
 
 
 #######################################################################
@@ -65,7 +65,7 @@ def run_svm_one_vs_rest_on_MNIST():
     return test_error
 
 
-print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
+# print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
 
 
 def run_multiclass_svm_on_MNIST():
@@ -81,7 +81,7 @@ def run_multiclass_svm_on_MNIST():
     return test_error
 
 
-print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
+# print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
 
 #######################################################################
 # 4. Multinomial (Softmax) Regression and Gradient Descent
